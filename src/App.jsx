68dab44487c5cdb7e61b6d3efa1130938b37cf2e
DeepakJadhav2003 <DeepakJadhav.drive@gmail.com>
2025-10-04 16:30:31 +0530

    Added Result Logic and Ouput

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         setuserInput((prevUserInpur) => {
             return {
                 ...prevUserInpur,
-                [inputIdentifier]:newValue
+                [inputIdentifier]: +newValue
             }
         })
     }
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,8 +1,41 @@
+import {calculateInvestmentResults , formatter} from "../util/investment.js"
 
 export default function Result({input}) {
+    let resultData = calculateInvestmentResults(input);
+    let initialInvestment = 
+        resultData[0].valueEndOfYear - resultData[0].interest - resultData[0].annualInvestment 
 
-    console.log(input)
+    console.log(resultData)
+
+
+
   return (
-    <p>...Result</p>
+    <table id="result">
+        <thead>
+            <tr>
+                <th>Year</th>
+                <th>Investment Value</th>
+                <th>Intrest (Value)</th>
+                <th>Total Intrest</th>
+                <th>Invested Capital</th>
+            </tr>
+        </thead>
+        <tbody>
+             {resultData.map((yearData) => {
+                const totalIntrest = yearData.valueEndOfYear - yearData.annualInvestment * yearData.year - initialInvestment;
+
+                const investCapital = yearData.valueEndOfYear - totalIntrest;
+                return(
+                    <tr key={yearData.year}>
+                        <td>{yearData.year}</td>
+                        <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                        <td>{formatter.format(yearData.interest)}</td>
+                        <td>{formatter.format(totalIntrest)}</td>
+                        <td>{formatter.format(investCapital)}</td>
+                    </tr> 
+                )
+             })}
+        </tbody>
+    </table>
   )
 }
